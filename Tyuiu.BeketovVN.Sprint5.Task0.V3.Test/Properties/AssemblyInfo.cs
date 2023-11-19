93c83de4ebacd3f9d9e160656526aac9380eec89
Avanor <avanor5242@yandex.ru>
2023-11-19 21:59:42 +0500

    Task0

---
+++ Tyuiu.BeketovVN.Sprint5.Task0.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BeketovVN.Sprint5.Task0.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BeketovVN.Sprint5.Task0.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd1a1b47-aced-4fa7-8f75-ff8ec862e941")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BeketovVN.Sprint5.Task0.V3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BeketovVN.Sprint5.Task0.V3.Lib;
+
+namespace Tyuiu.BeketovVN.Sprint5.Task0.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил Бекетов В.Н. | ИИПб-23-2";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                             *");
+            Console.WriteLine("* Задание #0                                                                   *");
+            Console.WriteLine("* Вариант #3                                                                   *");
+            Console.WriteLine("* Выполнил Бекетов В.Н.   | ИИПб-23-2                                          *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в       *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх   *");
+            Console.WriteLine("* знаков после запятой.                                                        *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            int x = 3;
+            Console.WriteLine("Значение переменной X = " + x);
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
