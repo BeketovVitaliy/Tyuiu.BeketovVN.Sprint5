104326e012aa15692a37b2fdf0cdef43dc229514
Avanor <avanor5242@yandex.ru>
2023-11-26 21:07:46 +0500

    Task4

---
+++ Tyuiu.BeketovVN.Sprint5.Task4.V24.Lib.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.BeketovVN.Sprint5.Task4.V24.Lib;
+
+namespace Tyuiu.BeketovVN.Sprint5.Task4.V24.Lib.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V24.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileexists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileexists);
+        }
+    }
+}
---
+++ Tyuiu.BeketovVN.Sprint5.Task4.V24/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BeketovVN.Sprint5.Task4.V24.Lib;
+
+namespace Tyuiu.BeketovVN.Sprint5.Task4.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Бекетов В.Н. | ИИПб-23-2";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла.                                     *");
+            Console.WriteLine("* Задание #4                                                                   *");
+            Console.WriteLine("* Вариант #24                                                                  *");
+            Console.WriteLine("* Выполнил  Бекетов В.Н.   | ИИПб-23-2                                         *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Написать программу, которая прочитывает значение из файла и подставляет      *");
+            Console.WriteLine("* вместо Х в формулу.                                                          *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            string path = $@"C:\DataSprint5\InPutDataFileTask4V1.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
